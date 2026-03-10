45f66ac048d9eb399d912fad9c3b5b3bcad40c1e
aditya-zanjad <adityazanjad474@gmail.com>
2026-03-10 23:58:32 +0530

    - Added test cases for the validation rule 'req' - Modified the code according this new validation rule test cases

--- src/Rules/Req.php
+++ src/Rules/Req.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace AdityaZanjad\Validator\Rules;
 
 use AdityaZanjad\Validator\Base\AbstractRule;
+use AdityaZanjad\Validator\Interfaces\MandatoryRule;
 
-class Req extends AbstractRule
+class Req extends AbstractRule implements MandatoryRule
 {
     public function validate(mixed $value): bool
     {
-        return !\is_null($value);
+        if ((\is_string($value) || \is_array($value)) && empty($value)) {
+            return false;
+        }
+
+        if (\is_null($value)) {
+            return false;
+        }
+
+        return true;
     }
 
     public function error(): string
---
+++ tests/unit/ReqRuleTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+use AdityaZanjad\Validator\Input;
+use AdityaZanjad\Validator\Rules\Req;
+use AdityaZanjad\Validator\Validator;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[UsesClass(Input::class)]
+#[UsesClass(Validator::class)]
+#[CoversClass(Req::class)]
+class ReqRuleTest extends TestCase
+{
+    public function testPasses(): void
+    {
+        $inputs = new Input([
+            'abc'   => '123',
+            'def'   => 'Hello World!',
+            'ghi'   =>  [1, 2, 3],
+            'jkl'   =>  (object) ['A', 'E', 'I', 'O', 'U'],
+            'mno'   =>  [null],
+            'pqr'   =>  'null'
+        ]);
+
+        $rules = [
+            'abc'   =>  'req',
+            'def'   =>  'req',
+            'ghi'   =>  'req',
+            'jkl'   =>  'req',
+            'mno'   =>  'req',
+            'pqr'   =>  'req',
+        ];
+
+        $validator = new Validator($inputs, $rules);
+        $validator->validate();
+
+        $this->assertNull($validator->errors()->firstOf('abc'));
+        $this->assertNull($validator->errors()->firstOf('def'));
+        $this->assertNull($validator->errors()->firstOf('ghi'));
+        $this->assertNull($validator->errors()->firstOf('jkl'));
+        $this->assertNull($validator->errors()->firstOf('mno'));
+        $this->assertNull($validator->errors()->firstOf('pqr'));
+        $this->assertFalse($validator->failed());
+    }
+
+    public function testFails(): void
+    {
+        $inputs = new Input([
+            'abc'   =>  '',
+            'def'   =>  [],
+            'ghi'   =>  null,
+        ]);
+
+        $rules = [
+            'abc'   =>  'req',
+            'def'   =>  'req',
+            'ghi'   =>  'req',
+            'jkl'   =>  'req'
+        ];
+
+        $validator = new Validator($inputs, $rules);
+        $validator->validate();
+
+        $this->assertIsString($validator->errors()->firstOf('abc'));
+        $this->assertIsString($validator->errors()->firstOf('def'));
+        $this->assertIsString($validator->errors()->firstOf('ghi'));
+        $this->assertIsString($validator->errors()->firstOf('jkl'));
+        $this->assertTrue($validator->failed());
+    }
+}
